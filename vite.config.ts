d157118d53dfb171a1b920ca7169cab41e028665
wusawusle <jailikespie@gmail.com>
2026-09-12 14:06:57 +0800

    Revert "initial"
    This reverts commit af63a62202b7574310e7f187f474eb02e8b3c69e.

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-// Served from https://OrionAF.github.io/iom-arcanist-optimizer/
-export default defineConfig({
-  base: '/arcanist-calc/',
-  plugins: [react()],
-});
